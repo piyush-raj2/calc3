29d80b55486780703bcceee8fe947e26a60860a5
Piyush Raj <piyush.raj@fractalanalytics.com>
2019-02-20 17:25:37 +0530

    Update calc.py
    print statement added

--- calc.py
+++ calc.py
@@ -12,6 +12,7 @@ def multiply(x,y):
 def main():
 	a = 7, b = 8
 	print("Addition operation for a = ", a, " and b = ", b, " is ", add(a,b))
+	print("Multiplication and substraction in one row below")
 	print("Substract operation for a =", a," and b = ", b, " is", subtract(a,b), " and multiplication for a=" , a," and b= ", b ," is ", multiply(a,b))
 
 if __name__ == '__main__':
