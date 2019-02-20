bfc6195972959de340c3d59f3091045b9a1009c7
Piyush Raj <piyush.raj@fractalanalytics.com>
2019-02-20 18:13:48 +0530

    Divide added

--- calc.py
+++ calc.py
@@ -10,11 +10,14 @@ def substract(x,y):
 def multiply(x,y):
 	return(x*y)
 
+def divide(x,y):
+	return(x/y)
 def main():
 	a = 7, b = 8
 	print("Addition operation for a = ", a, " and b = ", b, " is ", add(a,b))
 	print("Multiplication and substraction in one row below")
 	print("Substract operation for a =", a," and b = ", b, " is", subtract(a,b), " and multiplication for a=" , a," and b= ", b ," is ", multiply(a,b))
+	print("Divide operation for a =" , a, " and b = ", b, " is", divide(a,b))
 
 if __name__ == '__main__':
 	main()
